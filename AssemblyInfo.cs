7ebf1db7603591456b6ad874a57c0fc36c920984
ScriptsAndApps <99158605+ScriptsAndApps@users.noreply.github.com>
2022-02-19 02:15:08 -0800

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -20,7 +20,6 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("00000000-0000-0000-0000-projector000")]
 
 // Version information for an assembly consists of the following four values:
 //
